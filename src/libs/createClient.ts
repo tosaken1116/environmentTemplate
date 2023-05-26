589b339691083fa78eb41674df38ef273276309c
tosaken <v-ntosa@alterbooth.net>
2023-05-26 15:05:16 +0900

    graphql websocketの追加

--- src/libs/createClient.ts
+++ src/libs/createClient.ts
@@ -1,11 +1,24 @@
 import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
+import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
+import { createClient } from "graphql-ws";
 
+const httpLink = new HttpLink({
+    uri: process.env.NEXT_PUBLIC_BACKEND_URL,
+});
+
+const wsLink =
+    typeof window !== "undefined"
+        ? new GraphQLWsLink(
+              createClient({
+                  url: process.env.NEXT_PUBLIC_BACKEND_WS_URL!,
+              })
+          )
+        : httpLink;
 export const createApolloClient = () => {
     return new ApolloClient({
         ssrMode: typeof window === "undefined",
-        link: new HttpLink({
-            uri: process.env.NEXT_PUBLIC_BACKEND_URL,
-        }),
+        link: wsLink,
+
         cache: new InMemoryCache(),
     });
 };
